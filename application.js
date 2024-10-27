9a32e7e3ade42b8e8d1e763732848b5718ed3a0f
Ersultan Mansur <nullmansurov@gmail.com>
2024-10-28 00:34:24 +0500

    Update application.js

--- application.js
+++ application.js
@@ -3,7 +3,7 @@ let deferredPrompt;
 // Регистрация Service Worker
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
+        navigator.serviceWorker.register('service-worker.js')
             .then((registration) => {
                 console.log('Service Worker registered with scope:', registration.scope);
             })
