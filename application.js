6527a9adf15439371821bd02cdbd17571c42e736
Ersultan Mansur <nullmansurov@gmail.com>
2024-10-27 23:28:00 +0500

    Add files via upload

---
+++ application.js
@@ -0,0 +1,34 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+    document.getElementById('pwa-modal').style.display = 'flex'; // Изменено
+});
+
+document.getElementById('installBtn').onclick = () => {
+    if (deferredPrompt) {
+        deferredPrompt.prompt();
+        deferredPrompt.userChoice.then((choiceResult) => {
+            if (choiceResult.outcome === 'accepted') {
+                console.log('Пользователь установил PWA');
+            } else {
+                console.log('Пользователь отклонил установку PWA');
+            }
+            deferredPrompt = null;
+        });
+    }
+};
+
+document.getElementById('closeBtn').onclick = function () {
+    document.getElementById('pwa-modal').style.display = 'none'; // Изменено
+};
+
+window.onload = checkInstallApp;
+
+function checkInstallApp() {
+    const installApp = localStorage.getItem('installApp') === 'true';
+    if (!installApp) {
+        document.getElementById('pwa-modal').style.display = 'flex'; // Изменено
+    }
+}
